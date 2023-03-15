3b812d221f36581fed3b79b2aa5ae70226a8e34a
Jonathan Franks Oliveira <jonathan.franks011@gmail.com>
2023-03-15 16:57:59 -0300

    #

--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import Topo from './components/Topo';
 import Detalhes from './components/Detalhes';
 import Botao from "../../components/Botao";
+import Itens from './components/Itens';
+import { ScrollView } from 'react-native-web';
 
 export default function Cesta({topo, detalhes}) {
-    return <> 
-            <Topo {...topo}/> 
-            <View style={estilos.cesta}>
-            <Detalhes {...detalhes}/>
-            <Botao {...detalhes}/>
-        </View>        
-    </>
+    return <ScrollView> 
+                <Topo {...topo}/> 
+                <View style={estilos.cesta}>
+                    <Detalhes {...detalhes}/>
+                    <Botao {...detalhes}/>
+                    <Itens {...itens}/>
+                </View>        
+            </ScrollView>
 }
 
 const estilos = StyleSheet.create({
